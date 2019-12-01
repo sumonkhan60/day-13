07323f0300a06bfa85524b1227e3db33020642e7
sumonkhan60 <sumonkhan60@yahoo.com>
2019-12-01 20:36:34 +0600

    new folder and file added

---
+++ script.js
@@ -0,0 +1,63 @@
+$('#img1').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    // alert(img1SrcValue);
+    $('#mainImage').attr('src',imgSrcValue);
+});
+
+$('#img2').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+
+$('#img3').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+
+$('#img4').click(function () {
+    var imgSrcValue = $(this).attr('src');
+    $('#mainImage').attr('src',imgSrcValue);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createHeadingParagraphElement() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//     newHeadingElement.innerHTML = "This is a heading.";
+//     newParagraphElement.innerHTML = "This is a paragraph.";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+// }
+//
+// document.getElementById('btn').onclick = function () {
+//     createHeadingParagraphElement();
+// };
+
+// document.getElementById('btn').onclick = function () {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//
+//     newHeadingElement.innerHTML = "This is a heading.";
+//     newParagraphElement.innerHTML = "This is a paragraph.";
+//
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+// };
